f84904be8a11b6f3a49fc1ba9dcd72a420aaf6d3
Dolgikh Timur <timur.dolgix@mail.ru>
2021-03-23 21:47:02 +0500

    ClassCountry project

---
+++ 04_Encapsulation/02_Pojo/src/ru/skillbox/Country.java
@@ -0,0 +1,64 @@
+package ru.skillbox;
+
+public class Country {
+    private String nameCountry;
+    private int population;
+    private double countryArea;//км2
+    private String nameCapital;
+    private boolean outletToTheSea;
+
+    public Country(String nameCountry)
+    {
+        this.nameCountry = nameCountry;
+    }
+
+    public void setNameCountry(String nameCountry)
+    {
+        this.nameCountry = nameCountry;
+    }
+
+    public String getNameCountry()
+    {
+        return nameCountry;
+    }
+
+    public void setPopulation(int population)
+    {
+        this.population = population;
+    }
+
+    public int getPopulation()
+    {
+        return population;
+    }
+
+    public void setCountryArea(double countryArea)
+    {
+        this.countryArea = countryArea;
+    }
+
+    public double getCountryArea()
+    {
+        return countryArea;
+    }
+
+    public void setNameCapital(String nameCapital)
+    {
+        this.nameCapital = nameCapital;
+    }
+
+    public String getNameCapital()
+    {
+        return nameCapital;
+    }
+
+    public void setOutletToTheSea(boolean outletToTheSea)
+    {
+        this.outletToTheSea = outletToTheSea;
+    }
+
+    public boolean getOutletToTheSea()
+    {
+        return outletToTheSea;
+    }
+}
