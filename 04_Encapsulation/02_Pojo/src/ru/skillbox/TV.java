ba0386ca0d601415b10a23ceb5ec259310652675
Dolgikh Timur <timur.dolgix@mail.ru>
2021-03-23 22:07:06 +0500

    ClassTV project

---
+++ 04_Encapsulation/02_Pojo/src/ru/skillbox/TV.java
@@ -0,0 +1,54 @@
+package ru.skillbox;
+
+public class TV {
+    private String name;
+    private int price;
+    private int screenSize; //дюйм
+    private boolean color;
+
+    public TV(String name, int price)
+    {
+        this.name = name;
+        this.price = price;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setPrice(int price)
+    {
+        this.price = price;
+    }
+
+    public int getPrice()
+    {
+        return price;
+    }
+
+    public void setScreenSize(int screenSize)
+    {
+        this.screenSize = screenSize;
+    }
+
+    public int getScreenSize()
+    {
+        return screenSize;
+    }
+
+    public void setColor(boolean color)
+    {
+        this.color = color;
+    }
+
+    public boolean getColor()
+    {
+        return color = color;
+    }
+}
