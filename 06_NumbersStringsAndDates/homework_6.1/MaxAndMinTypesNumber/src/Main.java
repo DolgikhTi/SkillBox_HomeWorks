e6f9930b42441997e0921c9703e270b3a92d294b
Dolgikh Timur <timur.dolgix@mail.ru>
2021-04-02 11:53:16 +0500

    MaxAndMinTypesNumbers project

--- 06_NumbersStringsAndDates/homework_6.1/MaxAndMinTypesNumber/src/Main.java
+++ 06_NumbersStringsAndDates/homework_6.1/MaxAndMinTypesNumber/src/Main.java
@@ -10,9 +10,9 @@ public class Main
         System.out.println("Short.MAX_VALUE = " + Short.MAX_VALUE);
         System.out.println("Long.MIN_VALUE = " + Long.MIN_VALUE);
         System.out.println("Long.MAX_VALUE = " + Long.MAX_VALUE);
-        System.out.println("Float.MIN_VALUE = " + Float.MIN_VALUE);
+        System.out.println("Float.MIN_VALUE = " + -Float.MAX_VALUE);
         System.out.println("Float.MAX_VALUE = " + Float.MAX_VALUE);
-        System.out.println("Double.MIN_VALUE = " + Double.MIN_VALUE);
+        System.out.println("Double.MIN_VALUE = " + -Double.MAX_VALUE);
         System.out.println("Double.MAX_VALUE = " + Double.MAX_VALUE);
     }
 }
