2eb10b9be2067c20474a1f1bcd685ce86d6ba103
Dolgikh Timur <timur.dolgix@mail.ru>
2021-04-02 16:06:11 +0500

    Salary project

--- 06_NumbersStringsAndDates/homework_6.4/TotalEarningsCalculator/src/main/java/Main.java
+++ 06_NumbersStringsAndDates/homework_6.4/TotalEarningsCalculator/src/main/java/Main.java
@@ -4,6 +4,15 @@ public class Main {
 
     String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
     //TODO: напишите ваш код, результат вывести в консоль
-  }
+    int sum = 0;
 
+    for(int i = 0; i <= text.length() - 1; i++)
+    {
+      if(Character.isDigit(text.charAt(i)) && !Character.isDigit(text.charAt(i - 1)))
+      {
+        sum = sum + Integer.parseInt(text.substring(i, i + text.substring(i).indexOf(" ")).trim());
+      }
+    }
+    System.out.println(sum);
+  }
 }
