99cd120894bf2a6d3c92884738e36079be375990
Dolgikh Timur <timur.dolgix@mail.ru>
2021-04-02 14:44:26 +0500

    TruckContainerBox project

--- 06_NumbersStringsAndDates/homework_6.3/TrucksContainersAndBoxes/src/main/java/Main.java
+++ 06_NumbersStringsAndDates/homework_6.3/TrucksContainersAndBoxes/src/main/java/Main.java
@@ -3,8 +3,26 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        int truck = 1;
+        int container = 1;
         Scanner scanner = new Scanner(System.in);
-        String boxes = scanner.nextLine();
+        System.out.println("Введите количество ящиков:");
+        int boxes = Integer.parseInt(scanner.nextLine());
+        System.out.println("Грузовик: " + truck);
+        System.out.println("\tКонтейнер: " + container);
+        for (int i = 1; i <= boxes; i++) {
+            System.out.println("\t\tЯщик: " + i);
+            if (i % (12 * 27) == 0) {
+                truck += 1;
+                System.out.println("Грузовик: " + truck);
+            }
+            if (i % 27 == 0) {
+                container += 1;
+                System.out.println("\tКонтейнер: " + container);
+            }
+        }
+        System.out.println("Необходимо:\nгрузовиков - " + truck + "шт." + "\nконтейнеров - " + container + "шт.");
+
 
         // TODO: вывести в консоль коробки разложенные по грузовикам и контейнерам
         // пример вывода при вводе 2
