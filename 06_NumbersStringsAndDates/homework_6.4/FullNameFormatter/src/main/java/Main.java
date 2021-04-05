3f92adc6f2b0143b06833ef278e891c6fd0b1d28
Dolgikh Timur <timur.dolgix@mail.ru>
2021-04-05 19:36:18 +0500

    FullNameFormatter project

--- 06_NumbersStringsAndDates/homework_6.4/FullNameFormatter/src/main/java/Main.java
+++ 06_NumbersStringsAndDates/homework_6.4/FullNameFormatter/src/main/java/Main.java
@@ -9,13 +9,37 @@ public class Main
     while (true)
     {
       String input = scanner.nextLine();
-      if (input.equals("0")) {
+      if (input.equals("0"))
+      {
         break;
       }
-      //TODO:напишите ваш код тут, результат вывести в консоль.
-      //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
-      String name;
+      int count = 0;
+      int spaceIndex = input.indexOf(' ');
+      int spaceLastIndex = input.lastIndexOf(' ');
+
+      if(input.length() != 0)
+      {
+        count++;
+        for(int i = 0; i < input.length(); i++)
+        {
+          if(input.charAt(i) == ' ')
+          {
+            count++;
+          }
+        }
+      }
+      if(count != 3){
+        System.out.println("Введенная строка не является ФИО");
+        break;
+      }
+
+      String firstName = input.substring(0, spaceIndex);
+      String lastName = input.substring(spaceIndex + 1, spaceLastIndex);
+      String otherName = input.substring(spaceLastIndex + 1);
+
+      System.out.println("Фамилия: " + firstName);
+      System.out.println("Имя: " + lastName);
+      System.out.println("Отчество: " + otherName);
     }
   }
-
 }
