c2c97eac22da82739ad4f6879adde423aa50defc
Dolgikh Timur <timur.dolgix@mail.ru>
2021-04-05 19:18:46 +0500

    TruckContainerBox project

--- 06_NumbersStringsAndDates/homework_6.3/TrucksContainersAndBoxes/src/main/java/Main.java
+++ 06_NumbersStringsAndDates/homework_6.3/TrucksContainersAndBoxes/src/main/java/Main.java
@@ -3,40 +3,31 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        int truck = 1;
         int container = 1;
+        int truck = 1;
+        int boxInCont = 27;
+        int contInTruck = 12;
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите количество ящиков:");
-        int boxes = Integer.parseInt(scanner.nextLine());
-        System.out.println("Грузовик: " + truck);
-        System.out.println("\tКонтейнер: " + container);
-        for (int i = 1; i <= boxes; i++) {
-            System.out.println("\t\tЯщик: " + i);
-            if (i % (12 * 27) == 0) {
-                truck += 1;
-                System.out.println("Грузовик: " + truck);
+        int box = scanner.nextInt();
+        if (box > 0){
+            System.out.println("Грузовик: " + truck);
+            System.out.println("\tКонтейнер: " + container);
+            for (int i = 1; i <= box; i++) {
+                System.out.println("\t\tЯщик: " + i);
+                if (i % (contInTruck * boxInCont) == 0) {
+                    truck += 1;
+                    System.out.println("Грузовик: " + truck);
+                }
+                if (i % boxInCont == 0) {
+                    container += 1;
+                    System.out.println("\tКонтейнер: " + container);
+                }
             }
-            if (i % 27 == 0) {
-                container += 1;
-                System.out.println("\tКонтейнер: " + container);
-            }
+            System.out.println("Необходимо:\nгрузовиков - " + truck + " шт." + "\nконтейнеров - " + container + " шт.");
+
         }
-        System.out.println("Необходимо:\nгрузовиков - " + truck + "шт." + "\nконтейнеров - " + container + "шт.");
-
-
-        // TODO: вывести в консоль коробки разложенные по грузовикам и контейнерам
-        // пример вывода при вводе 2
-        // для отступа используйте табуляцию - \t
-
-        /*
-        Грузовик: 1
-            Контейнер: 1
-                Ящик: 1
-                Ящик: 2
-        Необходимо:
-        грузовиков - 1 шт.
-        контейнеров - 1 шт.
-        */
+        else {
+            System.out.println(" У Вас отсутствуют ящики");
+        }
     }
-
 }
