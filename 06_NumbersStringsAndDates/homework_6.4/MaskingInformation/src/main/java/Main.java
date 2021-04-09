1c6b4da9631df0e9e122c834aab096e8eb78b9ee
Dolgikh Timur <timur.dolgix@mail.ru>
2021-04-09 14:52:58 +0500

    MaskingInformation project

--- 06_NumbersStringsAndDates/homework_6.4/MaskingInformation/src/main/java/Main.java
+++ 06_NumbersStringsAndDates/homework_6.4/MaskingInformation/src/main/java/Main.java
@@ -7,7 +7,7 @@ public class Main {
         System.out.println(searchAndReplaceDiamonds("Номер кредитной карты <4008 1234 5678> 8912",
                 "***"));
         String str = scanner.nextLine();
-        String text = "< >";
+        String text = "<>";
         String hiddenText = text;
         while (hiddenText.contains("<") && hiddenText.contains(">"))
         {
@@ -20,9 +20,18 @@ public class Main {
         // TODO: реализовать метод, если в строке нет <> - вернуть строку без изменений
         int i = text.indexOf('<');
         int i1 = text.indexOf('>');
-        if (i > -1 & i1 > -1)
+
+        if (i > 0 & i1 > 0)
         {
             text = text.substring(0, i) + placeholder + text.substring(i1 + 1);
+        }
+
+        int i2 = text.indexOf('<');
+        int i3 = text.indexOf('>');
+
+        if (i2 > 0 & i3 > 0)
+        {
+            text = text.substring(0, i2) + placeholder + text.substring(i3 + 1);
         }
         return text;
     }
