ed3d4856519a20338712c1cbb88b9cbd26b055e8
Dolgikh Timur <timur.dolgix@mail.ru>
2021-04-12 17:52:28 +0500

    TotalEarningsCalculatorReqExp project

--- 06_NumbersStringsAndDates/homework_6.5/TotalEarningsCalculatorReqExp/src/main/java/Main.java
+++ 06_NumbersStringsAndDates/homework_6.5/TotalEarningsCalculatorReqExp/src/main/java/Main.java
@@ -6,7 +6,16 @@ public class Main {
 
   public static int calculateSalarySum(String text){
     //TODO: реализуйте метод
-    return -1;
+    String text1 = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
+    int sumDigit = 0;
+    for (String word : text.split(" ")) {
+      try {
+        sumDigit += Integer.parseInt(word);
+      } catch (NumberFormatException ignored) {
+      }
+    }
+    System.out.println(sumDigit);
+    return sumDigit;
   }
 
 }
