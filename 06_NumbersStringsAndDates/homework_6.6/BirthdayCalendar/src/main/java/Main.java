8e9fda59817dd2cb5601a14efc7b559b576782ef
Dolgikh Timur <timur.dolgix@mail.ru>
2021-04-15 16:05:28 +0500

    BirthdayCalendar project

--- 06_NumbersStringsAndDates/homework_6.6/BirthdayCalendar/src/main/java/Main.java
+++ 06_NumbersStringsAndDates/homework_6.6/BirthdayCalendar/src/main/java/Main.java
@@ -1,3 +1,7 @@
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Locale;
 
 public class Main {
 
@@ -16,7 +20,18 @@ public class Main {
         //TODO реализуйте метод для построения строки в следующем виде
         //0 - 31.12.1990 - Mon
         //1 - 31.12.1991 - Tue
+
+        StringBuilder builder = new StringBuilder();
+        SimpleDateFormat format = new SimpleDateFormat(" - dd.MM.yyyy - EE", Locale.ENGLISH);
+        Calendar calendar = new GregorianCalendar(year, month - 1, day, 0, 0, 0);
+        Calendar today = Calendar.getInstance();
+        int number = 0;
+        while (calendar.before(today)) {
+            builder.append(number).append(format.format(calendar.getTime())).append("\n");
+            calendar.add(Calendar.YEAR, 1);
+            number++;
+        }
         
-        return "";
+        return builder.toString();
     }
 }
