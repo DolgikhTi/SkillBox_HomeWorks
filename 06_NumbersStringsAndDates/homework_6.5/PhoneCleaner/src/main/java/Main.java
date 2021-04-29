2fb2a96e37f0923b20abca283c5ee83bcc0f1ecc
Dolgikh Timur <timur.dolgix@mail.ru>
2021-04-29 14:10:53 +0500

    PhoneCleaner project

--- 06_NumbersStringsAndDates/homework_6.5/PhoneCleaner/src/main/java/Main.java
+++ 06_NumbersStringsAndDates/homework_6.5/PhoneCleaner/src/main/java/Main.java
@@ -1,10 +1,9 @@
 import java.util.Scanner;
 
-public class Main {
-
-  private static final String REGEX_SPLIT_pHONE = "";
-
-  public static void main(String[] args) {
+public class Main
+{
+  public static void main(String[] args)
+  {
 
     Scanner scanner = new Scanner(System.in);
     while (true) {
@@ -13,7 +12,28 @@ public class Main {
         break;
       }
       //TODO:напишите ваш код тут, результат вывести в консоль.
+      String number = input.replaceAll("[^0-9]", "");
+      if (number.length() > 11 || number.length() < 10) {
+        System.out.println("Неверный формат номера");
+      } else {
+        if (number.length() == 10) {
+          String number2 = number.replaceAll("^", "7");
+          System.out.println(number2);
+        } else {
+          if (number.matches("^7\\d+")) {
+            System.out.println(number);
+          } else {
+            if (number.matches("^8\\d+")) {
+              String number3 = number.replaceAll("8", "7");
+              System.out.println(number3);
+            } else {
+              System.out.println("Неверный формат номера");
+            }
+          }
+        }
+      }
     }
   }
 
 }
+
