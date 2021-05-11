b64055ae6f8dc14c06cc7494c81dbae7d2b2ec92
Dolgikh Timur <timur.dolgix@mail.ru>
2021-05-11 13:05:18 +0500

    FullName(2) project

--- 06_NumbersStringsAndDates/homework_6.5/FullNameFormatterRegExp/src/main/java/Main.java
+++ 06_NumbersStringsAndDates/homework_6.5/FullNameFormatterRegExp/src/main/java/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
 
-  private static final String REGEX_SPLIT_FULL_NAME = "(([А-Я][а-я]+)(-[А-Я][а-я]+)?\\\\s+){2}(([А-Я][а-я]+)(\\\\s+[а-я]+)?)";
+  private static final String REGEX_SPLIT_FULL_NAME = "(([А-Я][а-я]+)(-[А-Я][а-я]+)?\\s+){2}(([А-Я][а-я]+)(\\s+[а-я]+)?)";
 
   public static void main(String[] args)
   {
@@ -15,6 +15,14 @@ public class Main {
       }
       //TODO:напишите ваш код тут, результат вывести в консоль.
       //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
+      if (input.matches(REGEX_SPLIT_FULL_NAME)) {
+        String[] fio = input.split("\\s+");
+        System.out.println("Фамилия: " + fio[0]);
+        System.out.println("Имя: " + fio[1]);
+        System.out.println("Отчество: " + fio[2]);
+      } else {
+        System.out.println("Введенная строка не является ФИО");
+      }
       }
   }
 
