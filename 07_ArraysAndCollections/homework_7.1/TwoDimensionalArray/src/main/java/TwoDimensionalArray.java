3e0541e914aaf6fe16b9431fa6e4d0b525217eb9
Dolgikh Timur <timur.dolgix@mail.ru>
2021-05-12 19:13:15 +0500

    TwoDimensionalArray project

--- 07_ArraysAndCollections/homework_7.1/TwoDimensionalArray/src/main/java/TwoDimensionalArray.java
+++ 07_ArraysAndCollections/homework_7.1/TwoDimensionalArray/src/main/java/TwoDimensionalArray.java
@@ -1,5 +1,6 @@
 public class TwoDimensionalArray {
     public static char symbol = 'X';
+    public static char symbol1 = ' ';
 
     public static char[][] getTwoDimensionalArray(int size) {
 
@@ -9,6 +10,16 @@ public class TwoDimensionalArray {
         // [ , X,  ]
         // [X,  , X]
 
-        return new char[0][0];
+        char[][] text = new char[size][size];
+        for (int i = 0; i < text.length; i++) {
+            for (int j = 0; j < text.length; j++) {
+                if (i == j || i + j == (size - 1)) {
+                    text[i][j] = symbol;
+                } else {
+                    text[i][j] = symbol1;
+                }
+            }
+        }
+        return text;
     }
 }
