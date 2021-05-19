fe13edf8d1688c301f949a239b80bf9650563da9
Dolgikh Timur <timur.dolgix@mail.ru>
2021-05-19 19:41:00 +0500

    Hospital project

--- 07_ArraysAndCollections/homework_7.1/Hospital/src/main/java/Hospital.java
+++ 07_ArraysAndCollections/homework_7.1/Hospital/src/main/java/Hospital.java
@@ -1,10 +1,20 @@
+import java.util.Arrays;
+
 public class Hospital {
+
+    public final static float MIN_HEALTH_TEMP = 36.2f;
+    public final static float MAX_HEALTH_TEMP = 36.9f;
 
     public static float[] generatePatientsTemperatures(int patientsCount) {
 
         //TODO: напишите метод генерации массива температур пациентов
 
-        return new float[0];
+        float[] patientTemperature = new float[patientsCount];
+        for (int i = 0; i < patientsCount; i++){
+            patientTemperature[i] = (float) (((Math.random() * 8) + 32) * 100) / (float) 100.0;
+        }
+
+        return patientTemperature;
     }
 
     public static String getReport(float[] temperatureData) {
@@ -13,11 +23,19 @@ public class Hospital {
             а также температуры всех пациентов.
         */
 
-        String report =
-                "Температуры пациентов: " + 0 +
-                        "\nСредняя температура: " + 0 +
-                        "\nКоличество здоровых: " + 0;
+        int count = 0;
+        float sum = 0.0f;
+        for (float temperatureData1 : temperatureData) {
+            if (temperatureData1 >= MIN_HEALTH_TEMP && temperatureData1 <= MAX_HEALTH_TEMP) {
+                count++;
+            }
+            sum += temperatureData1;
+        }
+        float average = sum / temperatureData.length;
+        return String.format(
+                "Температуры пациентов: %s\nСредняя температура: %.2f\nКоличество здоровых: %d",
+                Arrays.toString(temperatureData).
+                        replaceAll("^\\[|\\]$", "").replaceAll(",", ""), average, count);
 
-        return report;
     }
 }
