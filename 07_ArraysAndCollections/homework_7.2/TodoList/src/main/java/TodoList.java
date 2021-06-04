c86c7acae24b1e49af6236de677b08a18b46b4fc
Dolgikh Timur <timur.dolgix@mail.ru>
2021-06-04 16:28:49 +0500

    TodoList project

--- 07_ArraysAndCollections/homework_7.2/TodoList/src/main/java/TodoList.java
+++ 07_ArraysAndCollections/homework_7.2/TodoList/src/main/java/TodoList.java
@@ -2,28 +2,60 @@ import java.util.ArrayList;
 
 public class TodoList {
 
+    ArrayList<String> todoList = new ArrayList<>();
+
     public void add(String todo) {
         // TODO: добавьте переданное дело в конец списка
+
+        todoList.add(todo);
+
+        for (String deal : todoList) {
+            System.out.println(deal);
+        }
     }
 
     public void add(int index, String todo) {
         // TODO: добавьте дело на указаный индекс,
         //  проверьте возможность добавления
+
+        if (index < todoList.size()){
+            todoList.add(index, todo);
+            for (String deals : todoList){
+                System.out.println(deals);
+            }
+        }
+        if (index > todoList.size()){
+            todoList.add(todo);
+        }
     }
 
     public void edit(String todo, int index) {
         // TODO: заменить дело на index переданным todo индекс,
         //  проверьте возможность изменения
+
+        if (index <= todoList.size()){
+            todoList.set(index, todo);
+            for (String deal : todoList){
+                System.out.println(deal);
+            }
+        }
     }
 
     public void delete(int index) {
         // TODO: удалить дело находящееся по переданному индексу,
         //  проверьте возможность удаления дела
+
+        if (index < todoList.size()){
+            todoList.remove(index);
+            for (String deal : todoList){
+                System.out.println(deal);
+            }
+        }
     }
 
     public ArrayList<String> getTodos() {
         // TODO: вернуть список дел
-        return new ArrayList<>();
+        return todoList;
     }
 
 }
