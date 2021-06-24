1193daa5874247c43c145990f90a01b35975cfd5
Dolgikh Timur <timur.dolgix@mail.ru>
2021-06-24 20:54:36 +0500

    PhoneBook project

--- 07_ArraysAndCollections/homework_7.4/PhoneBook/src/main/java/PhoneBook.java
+++ 07_ArraysAndCollections/homework_7.4/PhoneBook/src/main/java/PhoneBook.java
@@ -9,25 +9,68 @@ public class PhoneBook {
     public void addContact(String phone, String name) {
         // проверьте корректность формата имени и телефона
         // если такой номер уже есть в списке, то перезаписать имя абонента
+        if (phone.matches("^[0-9]+$") && name.matches("^[a-zA-Zа-яА-Я]+$")){
         phoneBook.put(phone, name);
+        }
     }
 
     public String getNameByPhone(String phone) {
         // формат одного контакта "Имя - Телефон"
         // если контакт не найдены - вернуть пустую строку
-        return "";
+        StringBuilder nameWithPhones = new StringBuilder();
+
+        phoneBook.entrySet()
+                .stream()
+                .filter(pair -> pair.getKey().equals(phone))
+                .forEach(pair -> {
+                    if (nameWithPhones.length() == 0) {
+                        nameWithPhones.append(pair.getValue()).append(" - ");
+                    }
+                    nameWithPhones.append(pair.getKey()).append(" ");
+                });
+
+        return nameWithPhones.toString().trim();
     }
 
     public Set<String> getPhonesByName(String name) {
         // формат одного контакта "Имя - Телефон"
         // если контакт не найден - вернуть пустой TreeSet
-        return new TreeSet<>();
+        StringBuilder nameWithPhones = new StringBuilder();
+        TreeSet<String> phoneSet = new TreeSet<>();
+
+        phoneBook.entrySet()
+                .stream()
+                .filter(pair -> pair.getValue().equals(name))
+                .forEach(pair -> {
+                    if (nameWithPhones.length() == 0) {
+                        nameWithPhones.append(name).append(" - ");
+                    }
+                    nameWithPhones.append(pair.getKey()).append(" ");
+                });
+
+        if (nameWithPhones.length() > 0) {
+            phoneSet.add(nameWithPhones.toString().trim());
+        }
+
+        return phoneSet;
     }
 
     public Set<String> getAllContacts() {
         // формат одного контакта "Имя - Телефон"
         // если контактов нет в телефонной книге - вернуть пустой TreeSet
-        return new TreeSet<>();
+        TreeSet<String> phoneSet = new TreeSet<>();
+
+        phoneBook.forEach((key, value) -> {
+            StringBuilder builder = new StringBuilder();
+            builder.append(value).append(" - ");
+            phoneBook.entrySet()
+                    .stream()
+                    .filter(pair -> pair.getValue().equals(value))
+                    .forEach(pair -> builder.append(pair.getKey()).append(" "));
+            phoneSet.add(builder.toString().trim());
+        });
+
+        return phoneSet;
     }
 
 }
