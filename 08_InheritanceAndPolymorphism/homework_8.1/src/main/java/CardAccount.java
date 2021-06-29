311bdb5717028c30127dc68e566d8a73359780f4
Dolgikh Timur <timur.dolgix@mail.ru>
2021-06-29 15:34:16 +0500

    BankAccount project

--- 08_InheritanceAndPolymorphism/homework_8.1/src/main/java/CardAccount.java
+++ 08_InheritanceAndPolymorphism/homework_8.1/src/main/java/CardAccount.java
@@ -4,14 +4,6 @@ public class CardAccount extends BankAccount {
 
     @Override
     public void take(double amountToTake) {
-        double bank = amountToTake * 0.01;
-        if (getAmount() < amountToTake + bank){
-            System.out.println("Остатка на счёте " + getAmount() + "недостаточно для списания" + amountToTake);
-            System.out.println("  с учётом комисии в 1%: " + bank);
-        }else {
-            System.out.println("Cо счёта списана сумма: " + amountToTake);
-            System.out.println("Коммисия банка: " + bank);
-            setAmount(getAmount() - (amountToTake + bank));
-        }
+        super.take(amountToTake * 1.01);
     }
 }
--- 08_InheritanceAndPolymorphism/homework_8.1/src/main/java/DepositAccount.java
+++ 08_InheritanceAndPolymorphism/homework_8.1/src/main/java/DepositAccount.java
@@ -10,13 +10,9 @@ public class DepositAccount extends BankAccount {
 
     @Override
     public void put(double amountToPut) {
-        if (getAmount() > amountToPut){
-            System.out.println("Ошибка!");
-        }else {
-            setAmount(getAmount() + amountToPut);
-            lastIncome = LocalDate.now().plusMonths(31);
-            System.out.println("Пополнение счёта на сумму: " + amountToPut);
-        }
+        super.put(amountToPut);
+        lastIncome = LocalDate.now().plusMonths(31);
+        System.out.println("Пополнение счёта на сумму: " + amountToPut);
     }
 
 }
