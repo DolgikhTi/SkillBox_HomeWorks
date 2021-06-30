0f092284051e233965fffc4d4b2a86e862bc86f4
Dolgikh Timur <timur.dolgix@mail.ru>
2021-07-01 03:09:36 +0500

    GeometryCalculator project & TrucksContainersAndBoxes project

--- 07_NumbersAndDates/TrucksContainersAndBoxes/src/main/java/Main.java
+++ 07_NumbersAndDates/TrucksContainersAndBoxes/src/main/java/Main.java
@@ -2,23 +2,39 @@ import java.util.Scanner;
 
 public class Main {
 
+    static final int BOX_IN_CONTAINER = 27;
+    static final int CONTAINER_IN_TRUCK = 12;
+    static final int BOX_IN_TRUCK = BOX_IN_CONTAINER * CONTAINER_IN_TRUCK;
+
+    static final String EXIT_CODE = "exit";
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String boxes = scanner.nextLine();
+        while (scanner.hasNext()) {
+            String box = scanner.nextLine();
+            if (box.equals(EXIT_CODE)) {
+                return;
+            }
 
-        // TODO: вывести в консоль коробки разложенные по грузовикам и контейнерам
-        // пример вывода при вводе 2
-        // для отступа используйте табуляцию - \t
+            int boxCont = Integer.parseInt(box);
 
-        /*
-        Грузовик: 1
-            Контейнер: 1
-                Ящик: 1
-                Ящик: 2
-        Необходимо:
-        грузовиков - 1 шт.
-        контейнеров - 1 шт.
-        */
+            int containerCont = 0;
+            int truckCont = 0;
+
+            int i = 0;
+            while (i < boxCont) {
+                if (i % BOX_IN_TRUCK == 0) {
+                    System.out.println("Грузовик: " + ++truckCont);
+                }
+                if (i % BOX_IN_CONTAINER == 0) {
+                    System.out.println("\tКонтейнер: " + ++containerCont);
+                }
+                System.out.println("\t\tЯщик: " + ++i);
+            }
+
+            System.out.printf("Необходимо:%nгрузовиков - %d шт.%nконтейнеров - %d шт.%n",
+                    truckCont, containerCont);
+        }
     }
 
 }
