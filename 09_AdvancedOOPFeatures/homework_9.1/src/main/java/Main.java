908a47e5f45a9fffea0f8635c11dcfb3dd945eb7
Dolgikh Timur <timur.dolgix@mail.ru>
2021-07-02 17:26:43 +0500

    Default Changelist project

--- 09_AdvancedOOPFeatures/homework_9.1/src/main/java/Main.java
+++ 09_AdvancedOOPFeatures/homework_9.1/src/main/java/Main.java
@@ -1,8 +1,9 @@
+import java.util.Collections;
 import java.util.List;
 
 public class Main {
 
-    public static final String STAFF_TXT = "data/staff.txt";
+    private static final String STAFF_TXT = "data/staff.txt";
 
     public static void main(String[] args) {
         List<Employee> staff = Employee.loadStaffFromFile(STAFF_TXT);
@@ -11,5 +12,15 @@ public class Main {
 
     public static void sortBySalaryAndAlphabet(List<Employee> staff) {
         //TODO Метод должен отсортировать сотрудников по заработной плате и алфавиту.
+
+        Collections.sort(staff, (o1, o2) -> {
+
+            int salaryCompare = o1.getSalary().compareTo(o2.getSalary());
+                if (salaryCompare == 0){
+                    return o1.getName().compareTo(o2.getName());
+                }
+                return salaryCompare;
+        });
     }
+
 }
