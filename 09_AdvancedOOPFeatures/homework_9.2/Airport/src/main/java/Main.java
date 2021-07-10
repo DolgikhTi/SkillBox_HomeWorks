cfd867a8b894714076ebbb980bc5ed6999ee8065
Dolgikh Timur <timur.dolgix@mail.ru>
2021-07-10 14:30:24 +0500

    EmployeesAndAirport projects

--- 09_AdvancedOOPFeatures/homework_9.2/Airport/src/main/java/Main.java
+++ 09_AdvancedOOPFeatures/homework_9.2/Airport/src/main/java/Main.java
@@ -1,7 +1,10 @@
 import com.skillbox.airport.Airport;
 import com.skillbox.airport.Flight;
-import java.util.Collections;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,7 +14,19 @@ public class Main {
 
     public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
         //TODO Метод должден вернуть список рейсов вылетающих в ближайшие два часа.
-        return Collections.emptyList();
+
+        LocalDateTime dateNow = LocalDateTime.now();
+        return airport.getTerminals().stream()
+        .flatMap(terminal -> terminal.getFlights().stream())
+                .filter(f -> f.getType().equals(Flight.Type.DEPARTURE))
+                .filter(f -> f.getDate()
+                        .toInstant()
+                        .atZone(ZoneId.systemDefault())
+                        .toLocalDateTime().isAfter(dateNow) && f.getDate()
+                        .toInstant()
+                        .atZone(ZoneId.systemDefault())
+                        .toLocalDateTime().isBefore(dateNow.plusHours(2)))
+                .collect(Collectors.toList());
     }
 
 }
