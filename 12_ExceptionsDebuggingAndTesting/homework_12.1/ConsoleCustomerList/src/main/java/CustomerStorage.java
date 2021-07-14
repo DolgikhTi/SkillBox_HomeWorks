40d6121b4ebd40558c86d22736fbdf5e94a4f6ba
Dolgikh Timur <timur.dolgix@mail.ru>
2021-07-14 12:32:41 +0500

    ProtectionAgainstIncorrectData project

--- 12_ExceptionsDebuggingAndTesting/homework_12.1/ConsoleCustomerList/src/main/java/CustomerStorage.java
+++ 12_ExceptionsDebuggingAndTesting/homework_12.1/ConsoleCustomerList/src/main/java/CustomerStorage.java
@@ -4,6 +4,18 @@ import java.util.Map;
 public class CustomerStorage {
     private final Map<String, Customer> storage;
 
+    String nameFormat = "Имя: только рус/eng буквы (без пробелов и других символов)";
+    final String nameRegEx = "^[а-яА-яa-zA-Z]*$";
+
+    String surNameFormat = "Фамилия: только рус/eng буквы, возможно - через дефис (без пробелов и других символов))";
+    final String surNameRegEx = "^[а-яА-яa-zA-Z\\-]*$";
+
+    String phoneNumberFormat = "Телефон: в формате +79991234567 (10-13 цифр)";
+    final String phoneRegEx = "^\\+[\\d]{11,14}$";
+
+    String emailFormat = "E-mail: в формате name@domain.com (только eng буквы, дефисы, подчеркивания, точки, и один символ @)";
+    final String emailRegEx = "^([a-z0-9_-]+\\.)*[a-z0-9_-]+@[a-z0-9_-]+(\\.[a-z0-9_-]+)*\\.[a-z]{2,6}$";
+
     public CustomerStorage() {
         storage = new HashMap<>();
     }
@@ -14,8 +26,39 @@ public class CustomerStorage {
         final int INDEX_EMAIL = 2;
         final int INDEX_PHONE = 3;
 
+        if (!data.contains(" ")) {
+            throw new IllegalArgumentException(
+                    "Ошибка! Введен один компонент из четырех, или компоненты не разделены пробелами");
+        }
+
         String[] components = data.split("\\s+");
+
+        if (components.length > 4) {
+            throw new IllegalArgumentException("Ошибка! Введено более четырех компонентов");
+        }
+        if (components.length < 4) {
+            throw new IllegalArgumentException("Ошибка! Введено менее четырех компонентов");
+        }
+
+        if (!components[0].matches(nameRegEx)) {
+            throw new IllegalArgumentException("Ошибка! " + nameFormat);
+        }
+        if (!components[1].matches(surNameRegEx)) {
+            throw new IllegalArgumentException("Ошибка! " + surNameFormat);
+        }
+        if (!components[2].matches(emailRegEx)) {
+            throw new IllegalArgumentException("Ошибка! " + emailFormat);
+        }
+        if (!components[3].matches(phoneRegEx)) {
+            throw new IllegalArgumentException("Ошибка! " + phoneNumberFormat);
+        }
+
         String name = components[INDEX_NAME] + " " + components[INDEX_SURNAME];
+
+        if (storage.containsKey(name)) {
+            throw new IllegalArgumentException("Ошибка! В базе уже есть такая запись");
+        }
+
         storage.put(name, new Customer(name, components[INDEX_PHONE], components[INDEX_EMAIL]));
     }
 
