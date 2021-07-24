de2e329e78efa266cf836c63587e774a3080db11
Dolgikh Timur <timur.dolgix@mail.ru>
2021-07-24 12:40:30 +0500

    FolderSize project

--- 13_FilesAndNetwork/homework_13.1/src/main/java/FileUtils.java
+++ 13_FilesAndNetwork/homework_13.1/src/main/java/FileUtils.java
@@ -1,6 +1,26 @@
+import java.io.File;
+
 public class FileUtils {
 
     public static long calculateFolderSize(String path) {
-        return 0;
+
+        File file = new File(path);
+        long length = 0L;
+
+        for(File f : file.listFiles())
+        {
+            if(f.isDirectory())
+            {
+                for ( File child : f.listFiles())
+                {
+                    length = length + child.length();
+                }
+            }
+            else
+            {
+                length = f.length();
+            }
+        }
+        return length;
     }
 }
