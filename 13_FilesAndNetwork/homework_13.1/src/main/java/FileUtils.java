6217c99344deed02a14e9a78dd418d4868c4516b
Dolgikh Timur <timur.dolgix@mail.ru>
2021-07-25 12:04:33 +0500

    FolderSize project

--- 13_FilesAndNetwork/homework_13.1/src/main/java/FileUtils.java
+++ 13_FilesAndNetwork/homework_13.1/src/main/java/FileUtils.java
@@ -1,26 +1,35 @@
-import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class FileUtils {
 
     public static long calculateFolderSize(String path) {
-
-        File file = new File(path);
-        long length = 0L;
-
-        for(File f : file.listFiles())
-        {
-            if(f.isDirectory())
-            {
-                for ( File child : f.listFiles())
-                {
-                    length = length + child.length();
-                }
-            }
-            else
-            {
-                length = f.length();
-            }
+        try {
+            return Files.walk(Path.of(path))
+                    .filter(p -> p.toFile().isFile())
+                    .mapToLong(p -> p.toFile().length())
+                    .sum();
+        }catch (IOException e) {
+            return 0L;
         }
-        return length;
+    }
+
+    public static String sizeConverter(long originalSize) {
+
+        final String[] units = {" bytes", " Kb", " Mb", " Gb", " Tb", " Pb"};
+        int unitIndex = 0;
+
+        double tempSize = (double) originalSize / 1024;
+
+        while (tempSize >= 1) {
+            tempSize = tempSize / 1024;
+            unitIndex++;
+        }
+        if (unitIndex == 0) {
+            return originalSize + units[unitIndex];
+        } else {
+            return (Math.round(10 * originalSize / (Math.pow(1024, (unitIndex)))) / 10.0) + units[unitIndex];
+        }
     }
 }
