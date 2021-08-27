11fec237833511d9fb2b3cfef80556f3c03fadf3
Timur <timur.dolgix@mail.ru>
2021-08-27 17:30:09 +0500

    AvgCoursesPurchases project

--- 14_SQLAndHibernate/homework_14.1/src/main/java/Main.java
+++ 14_SQLAndHibernate/homework_14.1/src/main/java/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String url = "jdbc:mysql://localhost:3306/skillbox?serverTimezone=Europe/Moscow";
+        String url = "jdbc:mysql://localhost:3306/skillbox?serverTimezone=Europe/Moscow?autoReconnect=true&useSSL=false";
         String user = "root";
         String pass = "password";
 
